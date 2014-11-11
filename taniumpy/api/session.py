de78a2d881643f261b2863b18a4b6b8f3dfac516
Dan Hardy <danhardy@Dans-MacBook-Pro.local>
2014-11-11 08:54:48 -0800

    Add support for options values specification using **kwargs in Session findAll and save

--- taniumpy/api/session.py
+++ taniumpy/api/session.py
@@ -1,8 +1,17 @@
 import httplib
+import string
 import urllib
 
 from base64 import b64encode
 from taniumpy.object_types.base import BaseType
+
+
+class DynamicFormatter(string.Formatter):
+
+    def get_value(self, key, args, kwargs):
+        if type(key) == str:
+            return kwargs.get(key, '')
+        return string.Formatter.get_value(self, key, args, kwargs)
 
 class Session(object):
 
@@ -17,7 +26,7 @@ class Session(object):
         <object_list>
             {2}
         </object_list>
-        <options><flags></flags><hide_errors_flag></hide_errors_flag><include_answer_times_flag></include_answer_times_flag><row_counts_only_flag></row_counts_only_flag><aggregate_over_time_flag></aggregate_over_time_flag><most_recent_flag></most_recent_flag><include_hashes_flag></include_hashes_flag><hide_no_results_flag></hide_no_results_flag><use_user_context_flag></use_user_context_flag><script_data/><return_lists_flag></return_lists_flag><return_cdata_flag></return_cdata_flag><pct_done_limit></pct_done_limit><context_id></context_id><sample_frequency></sample_frequency><sample_start></sample_start><sample_count></sample_count><suppress_scripts></suppress_scripts><suppress_object_list></suppress_object_list><row_start>{3}</row_start><row_count>{4}</row_count><sort_order/><filter_string/><filter_not_flag></filter_not_flag><recent_result_buckets/><cache_id/><cache_expiration></cache_expiration><cache_sort_fields/><include_user_details></include_user_details><include_hidden_flag></include_hidden_flag><use_error_objects></use_error_objects><use_json>0</use_json><json_pretty_print></json_pretty_print><cache_filters/></options>
+        <options><flags>{flags}</flags><hide_errors_flag>{hide_errors_flag}</hide_errors_flag><include_answer_times_flag>{include_answer_times_flag}</include_answer_times_flag><row_counts_only_flag>{row_counts_only_flag}</row_counts_only_flag><aggregate_over_time_flag>{aggregate_over_time_flag}</aggregate_over_time_flag><most_recent_flag>{most_recent_flag}</most_recent_flag><include_hashes_flag>{include_hashes_flag}</include_hashes_flag><hide_no_results_flag>{hide_no_results_flag}</hide_no_results_flag><use_user_context_flag>{use_user_context_flag}</use_user_context_flag><script_data>{script_data}</script_data><return_lists_flag>{return_lists_flag}</return_lists_flag><return_cdata_flag>{return_cdata_flag}</return_cdata_flag><pct_done_limit>{pct_done_limit}</pct_done_limit><context_id>{context_id}</context_id><sample_frequency>{sample_frequency}</sample_frequency><sample_start>{sample_start}</sample_start><sample_count>{sample_count}</sample_count><suppress_scripts>{suppress_scripts}</suppress_scripts><suppress_object_list>{suppress_object_list}</suppress_object_list><row_start>{row_start}</row_start><row_count>{row_count}</row_count><sort_order>{sort_order}</sort_order><filter_string>{filter_string}</filter_string><filter_not_flag>{filter_not_flag}</filter_not_flag><recent_result_buckets>{recent_result_buckets}</recent_result_buckets><cache_id>{cache_id}</cache_id><cache_expiration>{cache_expiration}</cache_expiration><cache_sort_fields>{cache_sort_fields}</cache_sort_fields><include_user_details>{include_user_details}</include_user_details><include_hidden_flag>{include_hidden_flag}</include_hidden_flag><use_error_objects>{use_error_objects}</use_error_objects><use_json>{use_json}</use_json><json_pretty_print>{json_pretty_print}</json_pretty_print><cache_filters/></options>
     </typens:tanium_soap_request>
 </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>"""
@@ -43,18 +52,18 @@ class Session(object):
         finally:
             http.close()
 
-    def createGetObjectBody(self, object_type, row_start=None, row_count=None):
-        return self.REQUEST_BODY.format(self.session_id, self.GET_OBJECT, '<' + object_type.OBJECT_LIST_TAG + '/>', row_start, row_count)
+    def createGetObjectBody(self, object_type, **kwargs):
+        return DynamicFormatter().format(self.REQUEST_BODY, self.session_id, self.GET_OBJECT, '<' + object_type.OBJECT_LIST_TAG + '/>', **kwargs)
 
     def createUpdateObjectBody(self, obj):
-        return self.REQUEST_BODY.format(self.session_id, self.UPDATE_OBJECT, obj.toSOAPBody(), '', '')
+        return DynamicFormatter().format(self.REQUEST_BODY, self.session_id, self.UPDATE_OBJECT, obj.toSOAPBody(), **kwargs)
 
-    def findAll(self, object_type):
-        body = self.getResponse(self.createGetObjectBody(object_type))
+    def findAll(self, object_type, **kwargs):
+        body = self.getResponse(self.createGetObjectBody(object_type, **kwargs))
         return BaseType.fromSOAPBody(body)
 
-    def save(self, obj):
-        body = self.createUpdateObjectBody(obj)
+    def save(self, obj, **kwargs):
+        body = self.createUpdateObjectBody(obj, **kwargs)
         body = self.getResponse(body)
         return BaseType.fromSOAPBody(body)
 
--- taniumpy/test/test_request.py
+++ taniumpy/test/test_request.py
@@ -5,5 +5,6 @@ from taniumpy.object_types.user_list import UserList
 class TestRequest(unittest.TestCase):
 
     def test_requestUsers(self):
-        body = Session('', '').createGetObjectBody(UserList)
+        body = Session('', '').createGetObjectBody(UserList, row_count=1)
         self.assertTrue('<users/>' in body)
+        self.assertTrue('<row_count>1</row_count>' in body)
