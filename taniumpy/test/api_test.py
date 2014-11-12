2624d734bf2b977c48d2960cd8510e6c14463c96
Jim Olsen <jim.olsen@tanium.com>
2014-11-12 12:52:09 -0500

    remove interactive shell from api_test.py

--- taniumpy/test/api_test.py
+++ taniumpy/test/api_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -i
+#!/usr/bin/env python
 # -*- mode: Python; tab-width: 4; indent-tabs-mode: nil; -*-
 # ex: set tabstop=4
 # Please do not change the two lines above. See PEP 8, PEP 263.
