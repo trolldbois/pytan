08fc283546ab3f4a6efac5bcfdd7317b556f9a2f
Jim Olsen <jim.olsen@tanium.com>
2014-11-13 02:00:58 -0500

    make api_shell more betterer

--- taniumpy/test/api_shell.py
+++ taniumpy/test/api_shell.py
@@ -15,9 +15,92 @@ for aa in path_adds:
     if aa not in sys.path:
         sys.path.append(aa)
 
-import api
 
-# logging.basicConfig(level=logging.DEBUG)
+# adds readline, autocomplete, history to python interactive console
+import atexit
+import os
+import sys
+import pprint
+import code
+
+try:
+    import readline
+    import rlcompleter
+except:
+    pass
+
+sys.dont_write_bytecode = True
+
+
+def debug_list(debuglist):
+    for x in debuglist:
+        debug_obj(x)
+
+
+def debug_obj(debugobj):
+    pprint.pprint(vars(debugobj))
+
+
+# Utility function to dump all info about an object
+def introspect(obj, depth=0):
+    import types
+    print "%s%s: %s\n" % (depth * "\t", obj, [
+        x for x in dir(obj) if x[:2] != "__"])
+    depth += 1
+    for x in dir(obj):
+        if x[:2] == "__":
+            continue
+        subobj = getattr(obj, x)
+        print "%s%s: %s" % (depth * "\t", x, subobj)
+        if isinstance(subobj, types.InstanceType) and dir(subobj) != []:
+            introspect(subobj, depth=depth + 1)
+            print
+
+
+class HistoryConsole(code.InteractiveConsole):
+    def __init__(self, locals=None, filename="<console>",
+                 histfile=os.path.expanduser("~/.console-history")):
+        code.InteractiveConsole.__init__(self, locals, filename)
+        try:
+            self.init_history(histfile)
+        except:
+            pass
+
+    def init_history(self, histfile):
+        if 'libedit' in readline.__doc__:
+            # osx style readline
+            readline.parse_and_bind("bind ^I rl_complete")
+            readline.parse_and_bind("bind ^R em-inc-search-prev")
+        else:
+            # unix style readline
+            readline.parse_and_bind("tab: complete")
+        if hasattr(readline, "read_history_file"):
+            try:
+                readline.read_history_file(histfile)
+            except IOError:
+                pass
+            atexit.register(self.save_history, histfile)
+
+    @staticmethod
+    def save_history(histfile):
+        readline.write_history_file(histfile)
+
+
+console = HistoryConsole()
+
+logging.basicConfig(level=logging.DEBUG)
+
+# hide auth messages that are below warn (change to DEBUG to see them)
+logging.getLogger('api.session.auth').setLevel(logging.WARN)
+
+# show http debug messages
+logging.getLogger('api.session.http').setLevel(logging.WARN)
+
+# hide http.body messages that are below warn (change to DEBUG to see them)
+logging.getLogger('api.session.http.body').setLevel(logging.WARN)
+
+
+import api
 
 host = '172.16.31.128'
 username = 'Tanium User'
@@ -27,22 +110,3 @@ session = api.Session(host)
 print session
 session.authenticate(username, password)
 print session
-
-
-# v='''<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-# <SOAP-ENV:Body>
-#   <typens:tanium_soap_request xmlns:typens="urn:TaniumSOAP">
-#     <session>3-4813-ef4056a1daec52fa8d438edaa993c617e87ef09f2e62c1b3bb3a70ff833f73c6c436513abbb5e581969226a1cb9edf80206b5f822e8bd82654feeedf625cee27</session>
-#     <command>GetObject</command>
-#     <object_list>
-#       <action><id>1</id></action><action><id>2</id></action>
-#     </object_list>
-#   </typens:tanium_soap_request>
-# </SOAP-ENV:Body>
-# </SOAP-ENV:Envelope>
-# '''
-
-# b = session._getResponse(v)
-# print len(b)
-
-# all_sensors = session.find(api.SensorList())
