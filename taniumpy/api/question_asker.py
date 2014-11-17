267d49472d401ad2ce782f8607d19acf53ca90dd
Jim Olsen <jim.olsen@tanium.com>
2014-11-17 17:04:47 -0500

    Add str method to QuestionAsker
    Also make callback code easier to read

--- taniumpy/api/question_asker.py
+++ taniumpy/api/question_asker.py
@@ -4,6 +4,7 @@ import time
 class QuestionTimeoutException(Exception):
     pass
 
+
 class QuestionAsker(object):
     """A class to aid in asking a Question.
 
@@ -18,11 +19,8 @@ class QuestionAsker(object):
 
     POLLING_INTERVAL = 5
 
-    def __init__(self, session,
-            question,
-            polling_interval=None,
-            pct_complete_threshold=99,
-            timeout=300):
+    def __init__(self, session, question, polling_interval=None,
+                 pct_complete_threshold=99, timeout=300):
         self.session = session
         self.question = question
         self._polling_interval = polling_interval or self.POLLING_INTERVAL
@@ -30,6 +28,17 @@ class QuestionAsker(object):
         self._timeout = timeout
         self.result_info = None
         self._stop = False
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        str_tpl = "{} {}, timeout: {}, threshold: {}".format
+        ret = str_tpl(
+            class_name,
+            self.question,
+            self._timeout,
+            self.pct_complete_threshold,
+        )
+        return ret
 
     def setPctCompleteThreshold(self, val):
         self.pct_complete_threshold = val
@@ -64,30 +73,52 @@ class QuestionAsker(object):
         start = time.time()
         while not self._stop:
             if time.time() - start > self._timeout:
-                 raise QuestionTimeoutException()
+                raise QuestionTimeoutException()
             result_info = self.session.getResultInfo(self.question)
-            new_pct = (result_info.mr_tested * 100) / (result_info.estimated_total + .01)
-            if callbacks.get('ProgressChanged') and \
-                    (tested != result_info.tested or \
-                    passed != result_info.passed or \
-                    mr_tested != result_info.mr_tested or \
-                    mr_passed != result_info.mr_passed or \
-                    estimated_total != result_info.estimated_total or \
-                    pct != new_pct):
+
+            tested_pct = result_info.mr_tested * 100
+            estimated_total_pct = result_info.estimated_total + .01
+            new_pct = tested_pct / estimated_total_pct
+
+            tested_changed = tested != result_info.tested
+            passed_changed = passed != result_info.passed
+            mr_tested_changed = mr_tested != result_info.mr_tested
+            mr_passed_changed = mr_passed != result_info.mr_passed
+            est_total_changed = estimated_total != result_info.estimated_total
+            pct_changed = pct != new_pct
+
+            progress_changed = any([
+                tested_changed,
+                passed_changed,
+                mr_tested_changed,
+                mr_passed_changed,
+                est_total_changed,
+                pct_changed,
+            ])
+
+            answers_changed = any([
+                tested_changed,
+                passed_changed,
+            ])
+
+            if callbacks.get('ProgressChanged') and progress_changed:
                 callbacks['ProgressChanged'](self, new_pct)
+
             pct = new_pct
-            if callbacks.get('AnswersChanged') and ( \
-                    tested != result_info.tested or \
-                    passed != result_info.passed):
+
+            if callbacks.get('AnswersChanged') and answers_changed:
                 callbacks['AnswersChanged'](self, new_pct)
+
             if pct > self.pct_complete_threshold:
                 if callbacks.get('AnswersComplete'):
                     callbacks['AnswersComplete'](self, new_pct)
                 return
+
             tested = result_info.tested
             passed = result_info.passed
             mr_tested = result_info.mr_tested
             mr_passed = result_info.mr_passed
+
             if not self._stop:
                 time.sleep(self._polling_interval)
 
