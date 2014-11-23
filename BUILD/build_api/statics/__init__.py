6ee71812e5a36a9a19352a270fd19f828349ced4
Jim Olsen <jim.olsen@tanium.com>
2014-11-22 21:13:08 -0500

    Add ResultSet() to api init

--- BUILD/build_api/statics/__init__.py
+++ BUILD/build_api/statics/__init__.py
@@ -2,3 +2,4 @@ from .object_types.all_objects import *
 from .session import Session
 from .question_asker import QuestionAsker
 from .object_types.base import BaseType
+from .object_types.result_set import ResultSet
