07ccfb3447605d103f8d79025bd41b7edbb2b9a5
Jim Olsen <jim.olsen@tanium.com>
2015-02-06 18:41:30 -0500

    TSAT improvements
    add config.bat, run.bat, readme.md for TSAT
    add auto zipper for TSAT build

--- STATICWINBUILD/setup_Tanium_Sensor_Analysis_Tool.py
+++ STATICWINBUILD/setup_Tanium_Sensor_Analysis_Tool.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import zipfile
 
 my_file = os.path.abspath(sys.argv[0])
 my_dir = os.path.dirname(my_file)
@@ -15,6 +16,20 @@ for aa in path_adds:
 
 from distutils.core import setup
 import py2exe # noqa
+
+
+def write_file(filename, content):
+    fh = open(filename, 'w+')
+    fh.write(content)
+    fh.close()
+    print "Wrote %s" % (filename)
+
+
+def zipdir(path, zip):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zip.write(os.path.join(root, file),)
+
 
 VERSION = "1.0.2"
 DIST_DIR = "Tanium_Sensor_Analysis_Tool_dist_{}".format(VERSION)
@@ -45,3 +60,90 @@ setup(
 shutil.rmtree(os.path.join(my_dir, BUILD_DIR))
 
 # create batch file for running here and for configuration
+
+RUN_BATCH = ('''@echo off
+
+set my_dir=%~dp0%
+call %my_dir%\CONFIG.bat
+set cmdline=
+IF DEFINED username set cmdline=%cmdline% --username "%username%"
+IF DEFINED password set cmdline=%cmdline% --password "%password%"
+IF DEFINED host set cmdline=%cmdline% --host "%host%"
+IF DEFINED port set cmdline=%cmdline% --port "%port%"
+IF DEFINED loglevel set cmdline=%cmdline% --loglevel "%loglevel%"
+IF DEFINED output_dir set cmdline=%cmdline% --output_dir "%output_dir%"
+IF DEFINED sleep set cmdline=%cmdline% --sleep "%sleep%"
+IF DEFINED pct set cmdline=%cmdline% --pct "%pct%"
+IF DEFINED timeout set cmdline=%cmdline% --timeout "%timeout%"
+IF "%debugformat%" == "true" set cmdline=%cmdline% --debugformat
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+IF DEFINED platforms for %%a in (%platforms%) do (set cmdline=!cmdline! --platform "%%a")
+IF DEFINED categories for %%a in (%categories%) do (set cmdline=!cmdline! --category "%%a")
+
+cd %my_dir%
+Tanium_Sensor_Analysis_Tool.exe !cmdline!
+ENDLOCAL
+''')
+
+CONFIG_BATCH = ('''@echo off
+
+REM if username is not set here, it will be prompted for on the command line
+set username=
+
+REM if password is not set here, it will be prompted for on the command line
+set password=
+
+REM if host is not set here, it will be prompted for on the command line
+set host=
+
+REM optionally control which port to connect to (integer)
+set port=
+
+REM optionally control the logging / verbosity level (integer)
+set loglevel=
+
+REM optionally set the logging format to a more verbose logging format (true to enable)
+set debugformat=false
+
+REM optionally only ask questions for sensors that are in the given platforms
+REM space separated, i.e. Linux Mac Windows
+set platforms=
+
+REM optionally only ask questions for sensors that are in the given categories
+REM space separated, i.e. Reserved Tanium
+set categories=
+
+REM optionally control the output directory
+set output_dir=
+
+REM optionally override the default number of seconds to wait between asking questions
+set sleep=
+
+REM optionally override the default percent to consider question data complete
+set pct=
+
+REM optionally override the default number of seconds before questions timeout
+set timeout=
+''')
+
+README = ('''
+Tanium Sensor Analysis Tool
+===========================
+
+This tool is a statically compiled version of the PyTan toolset
+which will ask a question for every single sensor that exists
+in the Tanium Server.
+
+Modify CONFIG.bat according to your needs, then use RUN.bat to
+execute. Alternatively, just run Tanium_Sensor_Analysis_Tool.exe
+directly.
+''')
+
+write_file(os.path.join(DIST_DIR, 'RUN.bat'), RUN_BATCH)
+write_file(os.path.join(DIST_DIR, 'CONFIG.bat'), CONFIG_BATCH)
+write_file(os.path.join(DIST_DIR, 'README.md'), README)
+
+zf_name = DIST_DIR + '.zip'
+zf = zipfile.ZipFile(zf_name, 'w', zipfile.ZIP_DEFLATED)
+zipdir(DIST_DIR, zf)
