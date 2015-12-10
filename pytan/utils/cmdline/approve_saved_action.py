bbd58854a5a7cc4a9bc629c22b3d18cda3ef0a3a
Jim Olsen <jim.olsen@tanium.com>
2015-12-10 14:46:57 -0500

    approve_saved_action turtled

--- pytan/utils/cmdline/approve_saved_action.py
+++ pytan/utils/cmdline/approve_saved_action.py
@@ -1,52 +1,25 @@
+from . import base
 
 
-def approve_saved_action(doc):
-    """Method to setup the base :class:`CustomArgParse` class for command line scripts using :func:`base_parser`, then add specific arguments for scripts that use :mod:`pytan` to approve saved actions.
-    """
-    parser = parent_parser(doc=doc)
-    arggroup_name = 'Approve Saved Action Options'
-    arggroup = parser.add_argument_group(arggroup_name)
+class Worker(base.Base):
+    DESCRIPTION = 'Approve a saved action by ID'
+    GROUP_NAME = 'Approve Saved Action Options'
 
-    arggroup.add_argument(
-        '-i',
-        '--id',
-        required=True,
-        type=int,
-        action='store',
-        dest='id',
-        help='ID of Saved Action to approve',
-    )
+    def setup(self):
+        self.grp = self.parser.add_argument_group(self.GROUP_NAME)
+        self.grp.add_argument(
+            '-i', '--id',
+            required=True, type=int, action='store', dest='id',
+            help='ID of Saved Action to approve',
+        )
 
-    return parser
+    def get_response(self, kwargs):
+        response = self.handler.approve_saved_action(**kwargs)
+        print "++ Saved Action ID approved successfully: {0.id!r}".format(response)
+        return response
 
-
-
-
-def process_approve_saved_action_args(parser, handler, args):
-    """Process command line args supplied by user for approving a saved action
-
-    Parameters
-    ----------
-    parser : :class:`argparse.ArgParse`
-        * ArgParse object used to parse `all_args`
-    handler : :class:`pytan.handler.Handler`
-        * Instance of Handler created from command line args
-    args : args
-        * args object from parsing `parser`
-
-    Returns
-    -------
-    approve_action
-    """
-    q_args = {'id': args.id}
-
-    try:
-        approve_action = handler.approve_saved_action(**q_args)
-    except Exception as e:
-        traceback.print_exc()
-        print "\n\nError occurred: {}".format(e)
-        sys.exit(99)
-
-    print "++ Saved Action ID approved successfully: {0.id!r}".format(approve_action)
-    return approve_action
-
+    def get_result(self):
+        grps = [self.GROUP_NAME]
+        kwargs = self.get_parser_args(grps)
+        response = self.get_response(kwargs)
+        return response
