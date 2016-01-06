36edb75dfb4267c1a0a7ca11083ed4b70ef4cc23
Jim Olsen <jim.olsen@tanium.com>
2016-01-06 14:56:55 -0500

    new unix shell script for running tests

---
+++ new_test/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+my_path=$0
+my_script=`basename ${my_path}`
+my_dirname=`dirname ${my_path}`
+my_dir=`cd ${my_dirname} ; pwd`
+bin_dir=`cd ${my_dirname}/../bin ; pwd`
+
+export SHELL_DEBUG="True"
+
+. ${bin_dir}/config.sh
+
+export PYTHONPATH="${PYTHONPATH}:${pytan_ext_dir}"
+
+PYTEST_BINARY="${pytan_ext_dir}/pytest.py"
+
+cd "${my_dir}"
+spew "Executing: \"${PYTHON_BINARY}\" ${PYTHON_OPTIONS} \"${PYTEST_BINARY}\" ${PYTEST_OPTIONS} ${PASSED_ARGS}"
+"${PYTHON_BINARY}" ${PYTHON_OPTIONS} "${PYTEST_BINARY}" ${PYTEST_OPTIONS} "${@}"
+EXITCODE=$?
+spew "Exited with exit code: ${EXITCODE}"
+exit ${EXITCODE}
