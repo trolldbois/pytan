1c7908790887c180405502d6e7f0c80e59306dfd
Jim Olsen <jim.olsen@tanium.com>
2016-01-08 17:38:01 -0500

    added default options to always show extra information about skips/fails/etc

--- new_test/run_tests.sh
+++ new_test/run_tests.sh
@@ -4,7 +4,7 @@ my_path=$0
 my_script=`basename ${my_path}`
 my_dirname=`dirname ${my_path}`
 my_dir=`cd ${my_dirname} ; pwd`
-bin_dir=`cd ${my_dirname}/../bin ; pwd`
+bin_dir=`cd ${my_dir}/../bin ; pwd`
 
 export SHELL_DEBUG="True"
 
@@ -13,8 +13,10 @@ export SHELL_DEBUG="True"
 export PYTHONPATH="${PYTHONPATH}:${pytan_ext_dir}"
 
 PYTEST_BINARY="${pytan_ext_dir}/pytest.py"
+PYTEST_OPTIONS=-rsefwx
 
 cd "${my_dir}"
+
 spew "Executing: \"${PYTHON_BINARY}\" ${PYTHON_OPTIONS} \"${PYTEST_BINARY}\" ${PYTEST_OPTIONS} ${PASSED_ARGS}"
 "${PYTHON_BINARY}" ${PYTHON_OPTIONS} "${PYTEST_BINARY}" ${PYTEST_OPTIONS} "${@}"
 EXITCODE=$?
