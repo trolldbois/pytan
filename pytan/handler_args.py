500ec43dc12f62c88a7f963a7032b2da91c72c5e
Jim Olsen <jim.olsen@tanium.com>
2016-01-08 23:22:27 -0500

    handler_args updates
    make HANDLER_DEFAULTS the default for default_args everywhere, but
    allow it to be overridden via kwargs.
    make error classes be specific
    use str2bool in handle_string_arg

--- pytan/handler_args.py
+++ pytan/handler_args.py
@@ -4,8 +4,9 @@ import logging
 import getpass
 
 from pytan import PytanError, string_types, input
-from pytan.tickle.tools import obfuscate
 from pytan.store import ArgStore
+from pytan.tickle.tools import obfuscate
+from pytan.shellparser import str2bool
 from pytan.handler_logs import add_override_log
 
 from pytan.constants import HANDLER_DEFAULTS, PYTAN_KEY, ARGS_ORDER
@@ -13,24 +14,36 @@ from pytan.constants import HANDLER_DEFAULTS, PYTAN_KEY, ARGS_ORDER
 MYLOG = logging.getLogger(__name__)
 
 
+class ArgumentParsingError(PytanError):
+    pass
+
+
+class UserConfigError(PytanError):
+    pass
+
+
 def create_argstore(**kwargs):
     """pass."""
     add_override_log(**kwargs)
-    osenv_args = get_osenv_args()
+    pytan_key = kwargs.get('pytan_key', PYTAN_KEY)
+    default_args = kwargs.get('default_args', HANDLER_DEFAULTS)
+    args_order = kwargs.get('args_order', ARGS_ORDER)
+
+    kwargs['default_args'] = default_args
     config_file = determine_config_file(**kwargs)
     configfile_args = read_config_file(**kwargs)
-    default_args = HANDLER_DEFAULTS
+    osenv_args = get_osenv_args()
 
     argstore = ArgStore()
-    argstore.default_args = get_src_args('default_args', **default_args)
-    argstore.cmdline_args = get_src_args('cmdline_args', **kwargs)
-    argstore.osenvironment_args = get_src_args('osenvironment_args', **osenv_args)
-    argstore.configfile_args = get_src_args(config_file, **configfile_args)
     argstore.handler_args_source = ArgStore(_store_name='handler_args_source')
     argstore.handler_args = ArgStore(_store_name='handler_args')
+    argstore.default_args = ArgStore(_store_name='default_args', **default_args)
+    argstore.cmdline_args = get_src_args(_store_name='cmdline_args', **kwargs)
+    argstore.osenvironment_args = get_src_args(_store_name='osenvironment_args', **osenv_args)
+    argstore.configfile_args = get_src_args(_store_name=config_file, **configfile_args)
 
-    for arg, def_val in HANDLER_DEFAULTS.items():
-        arg_source = find_arg_source(arg, argstore)
+    for arg, def_val in default_args.items():
+        arg_source = find_arg_source(arg, argstore, args_order)
         val = argstore[arg_source][arg]
 
         if arg == 'password':
@@ -46,12 +59,12 @@ def create_argstore(**kwargs):
         def_val_is_str = isinstance(def_val, string_types)
 
         if val_is_str and not def_val_is_str:
-            val = handle_string_arg(arg, val)
+            val = handle_string_arg(arg, val, arg_source, def_val)
 
-        val = enforce_arg_type(arg, val, arg_source)
+        val = enforce_arg_type(arg, val, arg_source, def_val)
 
         if arg == 'password':
-            val = obfuscate(key=PYTAN_KEY, string=val)
+            val = obfuscate(key=pytan_key, string=val)
 
         argstore.handler_args[arg] = val
         argstore.handler_args_source[arg] = arg_source
@@ -96,15 +109,15 @@ def prompt_for_args(**kwargs):
     return handler_args
 
 
-def find_arg_source(arg, argstore):
-    for src in ARGS_ORDER:
+def find_arg_source(arg, argstore, args_order):
+    for src in args_order:
         if arg in argstore[src] and argstore[src].get(arg, None) is not None:
             arg_source = src
             break
     return arg_source
 
 
-def get_osenv_args():
+def get_osenv_args(**kwargs):
     osenv_args = {
         k.lower().replace('pytan_', ''): v
         for k, v in os.environ.items()
@@ -113,52 +126,53 @@ def get_osenv_args():
     return osenv_args
 
 
-def handle_string_arg(arg, value):
+def handle_string_arg(arg, value, arg_source, def_val):
     """handle string types that actually should be other types."""
     result = value
-    def_argtype = type(HANDLER_DEFAULTS[arg])
+    def_argtype = type(def_val)
     if def_argtype == bool:
-        valid = ["True", "False"]  # TODO bring in other trues/falses
-        if value.capitalize() in valid:
-            result = eval(value.capitalize())
-        else:
-            err = "Argument {!r} must be one of {!r}, supplied string containing {!r}"
-            err = err.format(arg, ','.join(valid), value)
-            raise PytanError(err)
+        try:
+            result = str2bool(value)
+        except Exception as e:
+            err = "Failed to evaluate string {!r} into a boolean from source {!r}, exception: {}"
+            err = err.format(value, arg_source, e)
+            raise ArgumentParsingError(err)
     if def_argtype == dict:
         try:
             result = dict(eval(value))
         except Exception as e:
-            err = "Tried to evaluate a dictionary from string {}, exception: {}"
-            err = err.format(value, e)
-            raise PytanError(err)
+            err = "Failed to evaluate string {!r} into a dictionary from source {!r}, exception {}"
+            err = err.format(value, arg_source, e)
+            raise ArgumentParsingError(err)
     return result
 
 
-def enforce_arg_type(arg, val, source):
+def enforce_arg_type(arg, val, source, def_val):
     pa_type = type(val).__name__
-    def_argtype = type(HANDLER_DEFAULTS[arg])
+    def_argtype = type(def_val)
     try:
         result = def_argtype(val)
     except Exception as e:
         err = "Argument {!r} value {!r} must be type {!r}, supplied type {!r} via {!r}: {}"
         err = err.format(arg, val, def_argtype.__name__, pa_type, source, e)
-        raise PytanError(err)
+        raise ArgumentParsingError(err)
     return result
 
 
 def get_src_args(_store_name, **kwargs):
     """pass."""
+    default_args = kwargs.get('default_args', HANDLER_DEFAULTS)
     src_args = ArgStore(_store_name=_store_name)
-    [setattr(src_args, k, kwargs[k]) for k in HANDLER_DEFAULTS if k in kwargs]
+    [setattr(src_args, k, kwargs[k]) for k in default_args if k in kwargs]
     return src_args
 
 
 def determine_config_file(**kwargs):
+    default_args = kwargs.get('default_args', HANDLER_DEFAULTS)
     osenv_args = get_osenv_args()
     cf_kwarg = kwargs.get('config_file', '')
     cf_env = osenv_args.get('config_file', '')
-    cf_def = HANDLER_DEFAULTS['config_file']
+    cf_def = default_args['config_file']
     config_file = cf_kwarg or cf_env or cf_def
     config_file = os.path.expanduser(config_file)
     return config_file
@@ -191,7 +205,7 @@ def write_config_file(puc_dict, **kwargs):
     except Exception as e:
         err = "Failed to write PyTan User config: '{}', exception: {}"
         err = err.format(config_file, e)
-        raise PytanError(err)
+        raise UserConfigError(err)
     else:
         m = "PyTan User config file successfully written: {} "
         m = m.format(config_file)
@@ -212,7 +226,7 @@ def read_config_file(**kwargs):
         except Exception as e:
             err = "PyTan User config file at: {} is invalid, exception: {}"
             err = err.format(config_file, e)
-            raise PytanError(err)
+            raise UserConfigError(err)
         else:
             m = "PyTan User config file successfully loaded: {} "
             m = m.format(config_file)
