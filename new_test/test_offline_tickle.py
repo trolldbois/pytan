b8662c498a8705cedbe57c814e7e7e45c146e554
Jim Olsen <jim.olsen@tanium.com>
2016-01-08 23:20:41 -0500

    more tickle tests

--- new_test/test_offline_tickle.py
+++ new_test/test_offline_tickle.py
@@ -18,29 +18,46 @@ my_dir = sys.path[0]
 xml_dir = os.path.join(my_dir, 'tanium_objects')
 
 action_statuses = {
-    'id': 1,
     'name': 'Action Statuses',
     'category': 'Reserved',
     'hash': 1792443391,
 }
 
 computer_name = {
-    'id': 3,
     'name': 'Computer Name',
     'category': 'Reserved',
     'hash': 3409330187,
 }
 
 tanium_action_log = {
-    'id': 638,
     'name': 'Tanium Action Log',
     'category': 'Tanium Diagnostics',
     'hash': 969736519,
 }
 
+operating_system_boot_directory = {
+    'name': 'Operating System Boot Directory',
+    'category': 'Operating System',
+    'hash': 1544486184,
+}
+
+operating_system_temp_directory = {
+    'name': 'Operating System Temp Directory',
+    'category': 'Operating System',
+    'hash': 2222730558,
+}
+
+questionlist_last_5_minutes = [
+    {'name': 'Disabled Firewall Profiles'},
+    {'name': 'Running Applications'},
+    {'name': 'Local User Information'},
+    {'name': 'Application Crashes Yesterday'},
+    {'name': 'Running Applications'},
+]
+
 raw_tanium_xmls = [
     {
-        'xml_file': 'sensors/Action Statuses.xml',
+        'xml_file': 'Action Statuses.xml',
         'obj_type': tanium_ng.SensorList,
         'obj_tag': 'sensors',
         'child_type': tanium_ng.Sensor,
@@ -48,7 +65,7 @@ raw_tanium_xmls = [
         'child_attrs': [action_statuses],
     },
     {
-        'xml_file': 'sensors/Computer Name.xml',
+        'xml_file': 'Computer Name.xml',
         'obj_type': tanium_ng.SensorList,
         'obj_tag': 'sensors',
         'child_type': tanium_ng.Sensor,
@@ -56,23 +73,35 @@ raw_tanium_xmls = [
         'child_attrs': [computer_name],
     },
     {
-        'xml_file': 'sensors/Tanium Action Log.xml',
+        'xml_file': 'Tanium Action Log.xml',
         'obj_type': tanium_ng.SensorList,
         'obj_tag': 'sensors',
         'child_type': tanium_ng.Sensor,
         'child_tag': 'sensor',
         'child_attrs': [tanium_action_log],
     },
+    {
+        'xml_file': 'Operating System Boot Directory and Operating System Temp Directory.xml',
+        'obj_type': tanium_ng.SensorList,
+        'obj_tag': 'sensors',
+        'child_type': tanium_ng.Sensor,
+        'child_tag': 'sensor',
+        'child_attrs': [operating_system_boot_directory, operating_system_temp_directory],
+    },
+    {
+        'xml_file': 'QuestionList_last_5_minutes.xml',
+        'obj_type': tanium_ng.QuestionList,
+        'obj_tag': 'questions',
+        'child_type': tanium_ng.Question,
+        'child_tag': 'question',
+        'child_attrs': questionlist_last_5_minutes,
+    },
+
 ]
 
 '''
-SensorList with 2
-QuestionList with 1
-QuestionList with 2
-UserList with 1
 UserList with 2
 ActionList with 2
-ActionList with 1
 '''
 
 pytest_raw_tanium_xmls = pytest.mark.parametrize("xml_dict", raw_tanium_xmls)
@@ -80,7 +109,7 @@ pytest_raw_tanium_xmls = pytest.mark.parametrize("xml_dict", raw_tanium_xmls)
 
 @pytest_raw_tanium_xmls
 def test_from_xml_raw_tanium(xml_dict):
-    xml_path = os.path.join(xml_dir, xml_dict['xml_file'])
+    xml_path = os.path.join(xml_dir, 'raw_tanium_response', xml_dict['xml_file'])
     xml = utils.read_file(xml_path)
     obj = tickle.from_xml(xml)
     assert isinstance(obj, tanium_ng.BaseType)
@@ -91,6 +120,6 @@ def test_from_xml_raw_tanium(xml_dict):
         assert isinstance(obj[idx], xml_dict['child_type'])
         for k, v in a.items():
             assert getattr(obj[idx], k) == v
-        if obj[idx].parameter_definition:
+        if getattr(obj[idx], 'parameter_definition', ''):
             assert isinstance(obj[idx].parameter_definition, text_type)
             assert isinstance(obj[idx].parameter_definition_dict, dict)
