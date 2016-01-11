f5a943092deff0e8e8fb1b3cd7c3a84d73ee469f
Jim Olsen <jim.olsen@tanium.com>
2016-01-11 17:33:57 -0500

    rename resultset specific methods from _report to _resultset
    also add XMLNS to ET’s namespace
    also add ElementType since cElementTree doesn’t expose it

--- pytan/tickle/__init__.py
+++ pytan/tickle/__init__.py
@@ -40,20 +40,26 @@ if fallback:
         except ImportError as e:
             m = "XML engine {} failed to import: {}"
             m = m.format(XML_ENGINES[XML_ENGINE], e)
-            MYLOG.debug(m)
+            MYLOG.warning(m)
 
 if ET is None:
     err = "Failed to import any XML Engine!"
     MYLOG.critical(err)
     raise XMLEngineError(err)
 
+import pytan
+from pytan.constants import XMLNS
+for k, v in XMLNS.items():
+    ET.register_namespace(k, v)
 
-from pytan.tickle.to_tree import to_xml, to_tree
-from pytan.tickle.to_dict import to_dict, to_json, to_csv
-from pytan.tickle.from_xml import from_xml, from_sse_xml
-from pytan.tickle.from_tree import from_tree
-from pytan.tickle.from_dict import from_dict, from_json
-from pytan.tickle.to_dict_report import to_dict_report, to_json_report, to_csv_report
+ElementType = Element = type(ET.Element(None))
+
+from pytan.tickle.to__tree import to_xml, to_tree
+from pytan.tickle.to__dict import to_dict, to_json, to_csv
+from pytan.tickle.to__dict_resultset import to_dict_resultset, to_json_resultset, to_csv_resultset
+from pytan.tickle.from__xml import from_xml, from_sse_xml
+from pytan.tickle.from__tree import from_tree
+from pytan.tickle.from__dict import from_dict, from_json
 from pytan import tanium_ng
 from pytan.tickle import tools
 
@@ -65,9 +71,9 @@ __all__ = [
     'to_dict',
     'to_json',
     'to_csv',
-    'to_dict_report',
-    'to_json_report',
-    'to_csv_report',
+    'to_dict_resultset',
+    'to_json_resultset',
+    'to_csv_resultset',
     'from_xml',
     'from_sse_xml',
     'from_tree',
@@ -75,7 +81,24 @@ __all__ = [
     'from_json',
 ]
 
+# expose all of the wrappers to pytan.tickle.tools for ease of access
+tools.to_xml = to_xml
+tools.to_tree = to_tree
+tools.to_dict = to_dict
+tools.to_json = to_json
+tools.to_csv = to_csv
+tools.to_dict_resultset = to_dict_resultset
+tools.to_json_resultset = to_json_resultset
+tools.to_csv_resultset = to_csv_resultset
+tools.from_xml = from_xml
+tools.from_sse_xml = from_sse_xml
+tools.from_tree = from_tree
+tools.from_dict = from_dict
+tools.from_json = from_json
+tools.ET = ET
 
+
+# expose all of the wrappers to pytan.tanium_ng.BaseType for ease of access
 def monkey_to_xml(self, **kwargs):
     """Serialize self into an XML body, relies on tickle"""
     result = to_xml(self, **kwargs)
@@ -100,21 +123,21 @@ def monkey_to_csv(self, **kwargs):
     return result
 
 
-def monkey_to_csv_report(self, **kwargs):
+def monkey_to_csv_resultset(self, **kwargs):
     """Serialize self into a CSV string, relies on tickle"""
-    result = to_csv_report(self, **kwargs)
+    result = to_csv_resultset(self, **kwargs)
     return result
 
 
-def monkey_to_json_report(self, **kwargs):
+def monkey_to_json_resultset(self, **kwargs):
     """Serialize self into a JSON string, relies on tickle"""
-    result = to_json_report(self, **kwargs)
+    result = to_json_resultset(self, **kwargs)
     return result
 
 
-def monkey_to_dict_report(self, **kwargs):
+def monkey_to_dict_resultset(self, **kwargs):
     """Serialize self into a list of dicts, relies on tickle"""
-    result = to_dict_report(self, **kwargs)
+    result = to_dict_resultset(self, **kwargs)
     return result
 
 
@@ -122,6 +145,7 @@ tanium_ng.BaseType.to_xml = monkey_to_xml
 tanium_ng.BaseType.to_dict = monkey_to_dict
 tanium_ng.BaseType.to_json = monkey_to_json
 tanium_ng.BaseType.to_csv = monkey_to_csv
-tanium_ng.ResultSetList.to_csv_report = monkey_to_csv_report
-tanium_ng.ResultSetList.to_json_report = monkey_to_json_report
-tanium_ng.ResultSetList.to_dict_report = monkey_to_dict_report
+tanium_ng.ResultSetList.to_csv_resultset = monkey_to_csv_resultset
+tanium_ng.ResultSetList.to_json_resultset = monkey_to_json_resultset
+tanium_ng.ResultSetList.to_dict_resultset = monkey_to_dict_resultset
+tanium_ng.BaseType._PYTAN = pytan
