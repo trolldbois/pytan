9f884da4475b731d1f4656642f640500c1f9fcb3
Daniel Belcher <daniel.belcher@tanium.com>
2016-02-19 23:13:52 -0600

    initial windows format content for pytan

---
+++ bin/config.ps1
@@ -0,0 +1,10 @@
+if (!$my_dir){
+    write-host "Need 'my_dir' defined to know where config.ps1 lives"
+    exit(99)
+}
+
+$parent_dir=(split-path -parent $my_dir)
+$pytan_ext_dir="$parent_dir\pytan\ext"
+
+$env:PYTHONPATH="$env:PYTHONPATH:$parent_dir"
+$env:PYTHON_VERSION=(python --version)
