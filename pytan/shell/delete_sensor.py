ffae46302eeee8378fc058a1c98049525f0780dd
Daniel Belcher <daniel.belcher@tanium.com>
2016-05-09 14:53:16 -0500

    added delete sensors

--- pytan/shell/delete_sensor.py
+++ pytan/shell/delete_sensor.py
@@ -1,5 +1,42 @@
 from . import base
 
 
-class Worker(base.DeleteBase):
-    OBJECT_TYPE = 'sensor'
+class Worker(base.Base):
+    DESCRIPTION = 'Searches for and deletes sensors.'
+    GROUP_NAME = 'Delete Sensor Options'
+    ACTION = 'delete'
+    PREFIX = 'delete_sensor'
+    NAME = 'sensor'
+
+    def setup(self):
+        self.grp = self.parser.add_argument_group(self.GROUP_NAME)
+
+        self.grp.add_argument(
+            '-s', '--sensor',
+            required=False, action='append', default=[], dest='search',
+            help='Searchable text string for finding {}'.format(self.NAME)
+        )
+        self.add_help_opts()
+        self.add_export_results_opts()
+        self.add_report_opts()
+        self.grp_choice_results()
+
+    def get_response(self, kwargs):
+        grps = [self.GROUP_NAME]
+        kwargs = self.get_parser_args(grps)
+        m = "++ Getting {} with search items:\n{}"
+        print(m.format(self.NAME, self.pf(kwargs)))
+        response = self.handler.delete_sensors(really_delete=True, **kwargs)
+        self.handler.MYLOG.debug("{}".format(response))
+        return response
+
+    def get_result(self):
+        grps = [self.GROUP_NAME]
+        kwargs = self.get_parser_args(grps)
+        response = None
+        try:
+            response = self.get_response(kwargs)
+            print("Deleted {}".format(response))
+        except:
+            print("Unable to find result for {}".format(self.pf(kwargs)))
+        return response
