6da010b53925f3a5a9b692566e44974255bae29e
Daniel Belcher <daniel.belcher@tanium.com>
2016-06-02 16:32:51 -0600

    config.ps1 updated to use winext.  compat work to come for 3.5 then back to tsat

--- bin/config.ps1
+++ bin/config.ps1
@@ -3,6 +3,7 @@ $PYTHON='python3'
 
 $parent_dir=(split-path -parent $my_dir)
 $pytan_ext_dir="$parent_dir\pytan\ext"
+$pytan_winext_dir="$parent_dir\pytan\winext"
 
-$env:PYTHONPATH="$env:PYTHONPATH:$parent_dir"
+$env:PYTHONPATH="$env:PYTHONPATH:$parent_dir;$pytan_winext_dir"
 $env:PYTHON_VERSION=(&$PYTHON --version)
