733b3e863b61ebf7d02faa2772209e65a0811896
Daniel Belcher <daniel.belcher@tanium.com>
2017-04-03 16:18:40 -0500

    cleaned up action_group workflow and cleaned up doc strings

--- lib/pytan/handler.py
+++ lib/pytan/handler.py
@@ -892,7 +892,6 @@ class Handler(object):
             * default: []
             * each string must describe a sensor and a filter which limits which computers the action will deploy `package` to
         action_group : str, optional
-            * default: u'default'
             * have an action group assigned to allow for proper scoping of actions
         action_options : str, list of str, optional
             * default: []
@@ -979,9 +978,6 @@ class Handler(object):
         # to deploy the action against
         action_filters = kwargs.get('action_filters', [])
 
-        # the name of the action group to deploy the action against
-        action_group = kwargs.get('action_group', u'default')
-
         # the question options to use on the pre-action question and on the
         # group for the action filters
         action_options = kwargs.get('action_options', [])
@@ -993,14 +989,13 @@ class Handler(object):
         action_option_defs = pytan.utils.dehumanize_question_options(action_options)
         package_def = pytan.utils.dehumanize_package(package)
 
-        clean_keys = ['package_def', 'action_filter_defs', 'action_option_defs', 'action_group']
+        clean_keys = ['package_def', 'action_filter_defs', 'action_option_defs']
         clean_kwargs = pytan.utils.clean_kwargs(kwargs=kwargs, keys=clean_keys)
 
         deploy_result = self._deploy_action(
             action_filter_defs=action_filter_defs,
             action_option_defs=action_option_defs,
             package_def=package_def,
-            action_group=action_group,
             **clean_kwargs
         )
         return deploy_result
@@ -2800,7 +2795,7 @@ class Handler(object):
         result = pytan.utils.xml_pretty(x=raw_xml, **clean_kwargs)
         return result
 
-    def _deploy_action(self, action_group, run=False, get_results=True, **kwargs):
+    def _deploy_action(self, run=False, get_results=True, **kwargs):
         """Deploy an action and get the results back
 
         This method requires in-depth knowledge of how filters and options are created in the API, and as such is not meant for human consumption. Use :func:`deploy_action` instead.
@@ -2816,7 +2811,6 @@ class Handler(object):
             * default: []
             * action filter option definitions
         action_group : str, optional
-            * default: u'default'
             * have an action group assigned to allow for proper scoping of actions
         start_seconds_from_now : int, optional
             * default: 0
@@ -2974,9 +2968,6 @@ class Handler(object):
             key='start_seconds_from_now', default=0, **clean_kwargs
         )
 
-        action_group = self.get(objtype=u'action_groups', name=action_group)
-        action_group_obj = action_group[0]
-
         expire_seconds = pytan.utils.get_kwargs_int(key='expire_seconds', **clean_kwargs)
 
         action_name_default = "API Deploy {0.name}".format(package_def['package_obj'])
@@ -3108,10 +3099,10 @@ class Handler(object):
         add_obj.approved_flag = 0
         add_obj.issue_count = 0
 
-        if 'default' not in action_group_obj.name:
-            action_obj = taniumpy.Action()
-            action_obj.action_group = action_group_obj
-            action_group_obj = action_obj.action_group
+        action_group = kwargs.get("action_group", None)
+
+        if action_group:
+            action_group_obj = self.get(objtype='action_groups', name=action_group)[0]
             add_obj.action_group = action_group_obj
 
         if action_filter_defs or action_option_defs:
