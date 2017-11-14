2489d4567021772dccc6b309015862e28ad09fc9
ljaqueme <ljaqueme@opentext.com>
2017-11-14 12:29:03 -0700

    packaging

---
+++ lib/pytan/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pytan',
+      version='2.2.2',
+      package_dir={'pytan': 'lib/pytan'},
+      packages=['.'],
+      )
---
+++ lib/taniumpy/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='taniumpy',
+      version='2.2.2',
+      package_dir={'taniumpy': 'lib/taniumpy'},
+      packages=['.', '.object_types',],
+      )
